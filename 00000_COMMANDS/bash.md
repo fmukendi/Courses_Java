b1e3db451a70e143ceda84dab8e2a7262e30e5b7
Unknown <franck.mukendi@gmail.com>
2019-11-20 01:03:18 -0500

    complete Hystrix

---
+++ 00000_COMMANDS/bash.md
@@ -0,0 +1,16 @@
+Bash CMD
+-----
+
+* (1) ```Zipkin```
+```bash
+RABBIT_URI=amqp://localhost java -jar zipkin.jar
+
+java -jar zipkin.jar
+```
+
+
+*(2)  ```rabbitmq```
+```bash
+export PATH=$PATH:/usr/local/opt/rabbitmq/sbin
+abbitmq-server
+```
--- 00002_Master Microservices/0004-SpringCloud/limits-service/src/main/java/com/mukeapps/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ 00002_Master Microservices/0004-SpringCloud/limits-service/src/main/java/com/mukeapps/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -3,6 +3,7 @@ package com.mukeapps.microservices.limitsservice.controller;
 
 import com.mukeapps.microservices.limitsservice.config.AppConfiguration;
 import com.mukeapps.microservices.limitsservice.model.LimitsConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +27,16 @@ public class LimitsConfigurationController {
     }
 
 
+    @GetMapping("/limits-with-fault-tolerance")
+    @HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+    public LimitsConfiguration retrieveLimitsFromConfigurationsForSure(){
+
+        throw  new RuntimeException("Not available");
+    }
+
+
+    public LimitsConfiguration fallbackRetrieveConfiguration(){
+
+        return  new LimitsConfiguration(9999, 9);;
+    }
 }
