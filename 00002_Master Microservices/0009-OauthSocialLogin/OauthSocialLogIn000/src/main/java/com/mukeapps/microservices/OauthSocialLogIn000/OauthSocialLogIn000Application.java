2697bc18ac6ca2c08a98f9976002088fb365ba01
Unknown <franck.mukendi@gmail.com>
2019-12-11 00:10:32 -0500

    push

---
+++ 00002_Master Microservices/0009-OauthSocialLogin/OauthSocialLogIn000/src/main/java/com/mukeapps/microservices/OauthSocialLogIn000/OauthSocialLogIn000Application.java
@@ -0,0 +1,17 @@
+package com.mukeapps.microservices.OauthSocialLogIn000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.mukeapps.microservices.OauthSocialLogIn000")
+public class OauthSocialLogIn000Application{
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSocialLogIn000Application.class, args);
+	}
+
+
+
+
+}
---
+++ 00002_Master Microservices/0009-OauthSocialLogin/OauthSocialLogIn000/src/test/java/com/mukeapps/microservices/OauthSocialLogIn000/OauthSocialLogIn000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukeapps.microservices.OauthSocialLogIn000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthSocialLogIn000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
